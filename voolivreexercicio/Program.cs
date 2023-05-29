df60f720312a31e225b776652526ec7eeb35e9a3
Kauan Markes <daluzmarkes@gmail.com>
2023-05-29 17:14:22 -0400

    01 commit

---
+++ voolivreexercicio/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class V
+{
+    static void Main(string[] args)
+    {
+       
+            VOO W = new VOO(01, new DateTime(2022, 07, 30));
+            Console.WriteLine($"Numero do Vôo: {W.numbervoo}");
+            Console.WriteLine($"Data do Vôo: {W.GetData()}\n\n");
+
+            Console.WriteLine($"Digite a poltrona desejada e veja se ela esta ocupado ou não: {W.Verifica(15)} \n\n");
+            Console.WriteLine($"Ocupar a Cadeira: {W.Ocupa(15)}\n");
+            Console.WriteLine($"Proxima poltrona Livre {W.ProximoLivre()}");
+
+
+
+            Console.WriteLine($"Numero de Vagas Livres: {W.Vagas()}");
+            
+
+        Console.ReadKey();
+    }
+}
---
+++ voolivreexercicio/voo.cs
@@ -0,0 +1,67 @@
+﻿public class VOO
+{
+    public int numbervoo;
+    public DateTime datahr = new DateTime();
+    public bool [] poltrona = new bool[100];
+
+   public VOO(int number, DateTime data)
+    {
+        this.numbervoo = number;
+        this.datahr = data;
+    }
+
+    public int ProximoLivre()
+    {
+        for (int i = 0; i < poltrona.Length; i++)
+        {
+            if (!poltrona[i])
+            {
+                return i + 1;
+            }
+        }
+        return -1 ;
+    }
+
+    public bool Verifica(int numPoltrona)
+    {
+        if(numPoltrona> 0 && numPoltrona < 100 && !poltrona[numPoltrona - 1])
+        {
+            poltrona[numPoltrona - 1] = true;
+            return true;
+        }
+        return false;
+    }
+
+    public bool Ocupa(int numpt)
+    {
+        if(numpt > 0 && numpt <= 100 && !poltrona[numpt - 1])
+        {
+            poltrona[numpt - 1] = true; 
+            return true;
+        }
+        return false;
+    }
+
+    public int Vagas()
+    {
+        int vagas = 0;
+
+        for(int i = 0; i < poltrona.Length; i++)
+        {
+            if (!poltrona[i])
+            {
+                vagas++;
+            }
+        }
+        return vagas;
+    }
+
+    public int GetVOO()
+    {
+        return numbervoo;
+    }
+    public DateTime GetData()
+    {
+        return datahr;
+    }
+}
